1da91067396342ceed94f4d6b19fb64f1d742a17
Naresh <nareshkr.22@gmail.com>
2018-01-23 14:42:57 +0530

    Second

---
+++ add_3_number.py
@@ -0,0 +1,9 @@
+num1 = input("Enter Number 1 : ")
+num2 = input("Enter Number 2 : ")
+num3 = input("Enter Number 3 : ")
+
+num_sum = float(num1) + float(num2) + float(num3)
+
+
+print("The sum of {0} + {1} + {2} is {3}".format(num1,num2,num3,num_sum))
+
