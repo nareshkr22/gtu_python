10ff124e2178ce06b859a9f4f5f7c47f6191d95d
Naresh <nareshkr.22@gmail.com>
2018-01-23 14:45:32 +0530

    Pract3

---
+++ largest_3.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter Number 1 : "))
+num2 = int(input("Enter Number 2 : "))
+num3 = int(input("Enter Number 3 : "))
+
+
+
+
+if num1 >= num2 and num1 >= num3:
+	largest = num1
+elif num2>=num1 and num2>=num3:
+	largest = num2
+else:
+	largest = num3
+
+
+print("The largest number among {0} , {1} , {2}  is {3}".format(num1,num2,num3,largest))
+	
