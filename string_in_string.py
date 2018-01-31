744eae4567a645f7d78807ca602f658989560ae5
nareshkr22 <nareshkr.22@gmail.com>
2018-01-31 14:56:39 +0530

    pract10

---
+++ string_in_string.py
@@ -0,0 +1,8 @@
+#Write a function that accepts 2 strings as arguments and returns TRUE if either string occurs anywhere in other
+
+
+str1 = raw_input("Enter String 1 : ")
+str2 = raw_input("Enter String 2 :")
+
+
+
