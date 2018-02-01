5e8c154a8839b1afe04994ae7d5a2de1c232c82e
nareshkr22 <nareshkr.22@gmail.com>
2018-02-01 13:36:46 +0530

    pract11

---
+++ fibbo.py
@@ -0,0 +1,16 @@
+def fibbo(num):
+    if num <= 1:
+        return num
+    else:
+        return fibbo(num-1) + fibbo(num-2)
+    
+num = int(input("Enter the number/limit : "))
+
+print("Fibbonaci Series upto {0}".format(num))
+a = 1
+b = 2 
+
+for i in range(num):
+     print(fibbo(i))
+
+
