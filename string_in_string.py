1708e17d2c930b446ea128ac31fede74e53e496f
nareshkr22 <nareshkr.22@gmail.com>
2018-02-01 14:55:43 +0530

    pract10

--- string_in_string.py
+++ string_in_string.py
@@ -1,8 +1,27 @@
 #Write a function that accepts 2 strings as arguments and returns TRUE if either string occurs anywhere in other
 
+word1 = input('Enter String : ')
+word2 = input('Enter String : ')
 
-str1 = raw_input("Enter String 1 : ")
-str2 = raw_input("Enter String 2 :")
+if(len(word1) < len(word2)):
+    temp = word2 
+    word2 = word1
+    word1 = temp
 
+    
 
-
+flag = ""
+for i in range(0, len(word1)):
+    if(word1[i] == word2[0]):        
+        for j in range(0, len(word2)):
+            if( len(word1) > i+j and word1[i+j] == word2[j]):
+                flag = "true"
+            else:
+                flag = "false"
+                    
+                
+    if(flag == "true"):
+        print("String is Exist in other String")
+        exit()
+    
+print("String is Not Exist in other String")
